9a7b3fde613620bc869e97c9af3f769ff96eeb97
Nelson Rodrigues <nelsonrds95@gmail.com>
2023-08-27 21:36:55 +0100

    initial commit

---
+++ src/test/java/dev/nelson/catalogconfigservice/catalogconfig/CatalogConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nelson.catalogconfigservice.catalogconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
